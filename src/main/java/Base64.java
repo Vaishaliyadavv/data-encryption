b191f0b7f678562da07e95aa37b7fa47caecb4a6
Vaishaliyadavv <vaishali.yadavv09@gmail.com>
2024-07-13 17:43:08 +0530

    initial commit

--- src/main/java/Base64.java
+++ src/main/java/Base64.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class Base64 {
+public class Base64 extends Encryption{
+
 }
--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,2 +1,3 @@
-package PACKAGE_NAME;public class CaesarCipher {
+public class CaesarCipher extends Encryption{
+
 }
